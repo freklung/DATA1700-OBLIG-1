de17d9c37ceec49cc28aa361c6a7a48055a8e745
freklung <144021596+freklung@users.noreply.github.com>
2024-02-12 14:35:02 +0100

    tried fixing array problems. doesnt work

--- DATA1700-OBLIG-1/src/main/resources/static/billettkjøp.js
+++ DATA1700-OBLIG-1/src/main/resources/static/billettkjøp.js
@@ -4,11 +4,11 @@ function kjoepBillett(){
     //initaliserer et array
 
     //let fValg = document.getElementById("filmvalg")
-    let antall = document.getElementById("antall").value;
-    let fNavn = document.getElementById("fNavn").value;
-    let eNavn = document.getElementById("eNavn").value;
-    let tlfNr = document.getElementById("tlfNr").value;
-    let epost = document.getElementById("epost").value;
+    let antall = this.document.getElementById("antall").value;
+    let fNavn = this.document.getElementById("fNavn").value;
+    let eNavn = this.document.getElementById("eNavn").value;
+    let tlfNr = this.document.getElementById("tlfNr").value;
+    let epost = this.document.getElementById("epost").value;
 
     //legger verdier inn i arrayet
     billettArray.push(antall,fNavn,eNavn,tlfNr,epost);
@@ -17,14 +17,24 @@ function kjoepBillett(){
     localStorage.setItem("billettArray", JSON.stringify(billettArray));
 
     //Skriver ut array
-    console.log("Antall billetter: " + JSON.stringify(billettArray[0]) + ", Fornavn: " + billettArray[1] +
+    console.log("Antall billetter: " + localStorage.getItem(billettArray[0]) + ", Fornavn: " + billettArray[1] +
                 ", Etternavn: " + billettArray[2] + ", Telefonnummer: " + billettArray[3] +
                 ", Epost: " + billettArray[4]);
 
+    //Fjerner tekst fra input-feltene
     document.getElementById("antall").value = ""
     document.getElementById("fNavn").value = ""
     document.getElementById("eNavn").value = ""
     document.getElementById("tlfNr").value = ""
     document.getElementById("epost").value = ""
 
+    document.getElementById("alleBilletter").innerHTML = parsed;
+    $("#alleBilletter").html(parsed);
+}
+
+function slettAlle(){
+    localStorage.getItem(billettArray)
+    for (i = 0; i < billettArray.length; i++){
+        console.log(billettArray[i])
+    }
 }
