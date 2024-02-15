25322e33e89bbed79a0dffea8b2b65d6f8a2bb07
freklung <144021596+freklung@users.noreply.github.com>
2024-02-16 00:41:10 +0100

    fikset slik at visarray printer ut arrayets innhold på en mer strukturert og lesbar måte la til kommentarer på det meste

--- DATA1700-OBLIG-1/src/main/resources/static/billettkjøp.js
+++ DATA1700-OBLIG-1/src/main/resources/static/billettkjøp.js
@@ -23,43 +23,60 @@ function kjoepBillett(){
     document.getElementById("tlfNr").value = ""
     document.getElementById("epost").value = ""
 
+    //kaller den neste funksjonen
     visArray()
 }
 
-function visArray(){
+function visArray() {
     var visArray = document.getElementById("alleBilletter");
-    //clearer listen slik at det ikke vises noe før vi vil det
-    visArray.innerHTML = " ";
-    //skaper et nytt element i listen i html med text fra arrayet
-    array.forEach(function(input) {
-        var nyBillett = document.createElement("p")
-        nyBillett.textContent = input;
+    visArray.innerHTML = " "; // Clear the list
+
+    // lager en ny paragraph for hvert element i arrayet
+    array.forEach((input, nummer) => {
+        var nyBillett = document.createElement("p");
+        var label = '';
+        // setter format for printing av arrayets innhold
+        switch (nummer %   6) {
+            case  0: label = 'fValg: '; break
+            case  1: label = 'Antall: '; break
+            case  2: label = 'fNavn: '; break
+            case  3: label = 'eNavn: '; break
+            case  4: label = 'tlfNr: '; break
+            case  5: label = 'epost: '; break
+        }
+        //definerer innholdet til elementet nybillett
+        nyBillett.textContent = label + input
         visArray.appendChild(nyBillett)
-    })
+    });
 }
 
 function slettBilletter(){
+    // sletter alt fra localstorage
     localStorage.clear()
+    //Sletter html innhold forbundet med id alleBilletter
     var slettArray = document.getElementById("alleBilletter")
     slettArray.replaceWith(" ");
 }
 
+//venter til nettsiden er lastet inn med å kjøre skriptet. uten dette vil vi bare få masse typeerror meldinger
 document.addEventListener("DOMContentLoaded", function (){
 //Oppretter const data for inputfeltene i html filen
 const antallValid = document.getElementById("antall")
 const fNavnValid = document.getElementById("fNavn")
 const eNavnValid = document.getElementById("eNavn")
 const tlfNrValid = document.getElementById("tlfNr")
-const epostValid = document.getElementById("epost")
+const epostValid = document.getElementById("epost");
 
+// oppretter const data for hvert felt av errormeldingene i html
 const antallerror = document.getElementById("antallerror")
 const fnavnerror = document.getElementById("fnavnerror")
 const enavnerror = document.getElementById("eNavnerror")
 const tlferror = document.getElementById("tlferror")
-const eposterror = document.getElementById("eposterror")
+const eposterror = document.getElementById("eposterror");
 
+// definerer regex for hvordan epost og
 const tlfRegexp = /[0-9]/;
-const epostRegexp = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+const epostRegexp = /\S+@\S+\.\S+/;
 function validerInput(inputFelt, feilMld, regex) {
     inputFelt.addEventListener("blur", () => {
         feilMld.textContent = "";
@@ -69,14 +86,18 @@ function validerInput(inputFelt, feilMld, regex) {
     });
 }
 
+//sjekker at antall ikke er lik 0
 function validerNot0(inputfelt, feilmld){
     inputfelt.addEventListener("blur", () =>{
+        //setter feilmeldingen til å defaulte til å være blank
         feilmld.textContent = "";
+        //hvis man angir antall billetter til å være lik eller mindre vil siden si ifra om dette
         if (inputfelt >= 0){
             feilmld.textContent = "Du må velge antall billetter";
         }
 });}
 
+//kaller valideringsfunksjonene på de forskjellige inputfeltene
 validerNot0(antallValid, antallerror)
 validerInput(fNavnValid, fnavnerror, /^[a-zA-Z]/)
 validerInput(eNavnValid, enavnerror, /^[a-zA-Z]/ )
